69c3d97cbd7dd0de84ba68970f99384879bffa6a
Colin <speedtouch1733@gmail.com>
2022-11-27 14:06:35 +1100

    added test files with different cases

---
+++ reverse_string/reverse_test.py
@@ -0,0 +1,14 @@
+from reverse import reverse_string
+
+def test_reverse():
+    assert reverse_string("dictionary") == "yranoitcid"
+    assert reverse_string("hello") == "olleh"
+    assert reverse_string("12345") == "54321"
+
+
+
+if __name__ == "__main__":
+    test_reverse()
+    print("Everything passed :)")
+
+
